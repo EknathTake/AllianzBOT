f6f869ed98f5700896589f9640afc19695e07f3f
Eknath Take <eknathtake@gmail.com>
2018-09-25 12:08:42 +0530

    Unused packages removed

--- src/main/java/com/allianzbot/process/impl/AllianzBotProcessImpl.java
+++ src/main/java/com/allianzbot/process/impl/AllianzBotProcessImpl.java
@@ -5,9 +5,7 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.TreeSet;
@@ -44,7 +42,6 @@ import com.allianzbot.service.interfaces.IAllianzBotOpenNlpService;
 import com.allianzbot.service.interfaces.IAllianzBotSolrService;
 import com.allianzbot.service.interfaces.IAllianzBotTextExtractorService;
 import com.allianzbot.service.interfaces.IAllianzBotTokenizerService;
-import com.allianzbot.utils.AllianzBotConstants;
 import com.allianzbot.utils.AllianzBotContentTypes;
 import com.allianzbot.utils.FileUtils;
 
